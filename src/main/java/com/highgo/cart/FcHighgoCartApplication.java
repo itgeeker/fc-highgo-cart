9c7573a9fda422973eaa9df96c3aaa0e4581528e
huangzhiwei <huang.zhiwei@jyall.com>
2016-05-12 18:52:01 +0800

    Initial commit

---
+++ src/main/java/com/highgo/cart/FcHighgoCartApplication.java
@@ -0,0 +1,12 @@
+package com.highgo.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcHighgoCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcHighgoCartApplication.class, args);
+	}
+}
